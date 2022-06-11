1fe3ca729a4c6e4730d2035446b288f205bf836a
Omar Ochoa <omar@ochoawebdesign.com>
2022-06-10 21:57:49 -0700

    Basic api js setup

---
+++ app.js
@@ -0,0 +1,19 @@
+document.querySelector('.get-jokes').addEventListener('click', getJokes);
+
+function getJokes(e) {
+    const number = document.querySelector('input[type=number]').value;
+
+    const xhr = new XMLHttpRequest();
+
+    xhr.open('GET', `http://api.icndb.com/jokes/random/${number}`, true);
+
+    xhr.onload = function() {
+        if (this.status === 200) {
+            const response = this.responseText;
+            console.log(response);
+        }
+    }
+    xhr.send();
+
+    e.preventDefault();
+}
